ecbe35434b4450e42deac643af723a0bfe638268
Clayson Caitano da Silva <clayson.caitano@gmail.com>
2025-08-20 16:42:09 -0300

    Add project files.

---
+++ OracleCloudAPITest/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OracleCloudAPITest.Controllers
+{
+    [Route("Home")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return Ok("Deu Certo!");
+        }
+    }
+}
